7d78a5d38864ac6392e0e96fbc60fc0146366390
fry1231 <sobaka.samsobaka@yandex.ru>
2024-04-01 01:25:13 +0200

    [Fix] remove dev support

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+// import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
+    {/*<DevSupport ComponentPreviews={ComponentPreviews}*/}
+    {/*            useInitialHook={useInitial}*/}
+    {/*>*/}
       <App/>
-    </DevSupport>
+    {/*</DevSupport>*/}
   </BrowserRouter>
   // <React.StrictMode>
   //   <BrowserRouter>
