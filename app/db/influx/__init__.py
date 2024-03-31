654eb5fad74b04c3c0bdc7bf94b55484d0e86d98
fry1231 <sobaka.samsobaka@yandex.ru>
2024-03-31 03:24:56 +0200

    [Feature] Submitting farm sensors with jwt, watering endpoint

--- app/db/influx/__init__.py
+++ app/db/influx/__init__.py
@@ -1,4 +1,5 @@
 from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBClientError
 from config import INFLUXDB_HOST, INFLUXDB_PASSWORD, INFLUXDB_USERNAME, INFLUXDB_PORT, logger
 from db.influx.client import InfluxClient
 from db.influx.crud import get_influx_data, write_influx_data
@@ -24,10 +25,17 @@ for db_name in ['home', 'farm']:
                                               replication='1')
         logger.info(f'Database {db_name} created in InfluxDB')
     else:
-        influx_client.alter_retention_policy(name=db_name + '_ret_policy',
-                                             database=db_name,
-                                             duration=retention_policies[db_name],
-                                             replication=1)
         logger.info(f'Database {db_name} already exists in InfluxDB')
+        try:
+            influx_client.alter_retention_policy(name=db_name + '_ret_policy',
+                                                 database=db_name,
+                                                 duration=retention_policies[db_name],
+                                                 replication=1)
+            logger.info(f'Retention policy for {db_name} updated')
+        except InfluxDBClientError:
+            influx_client.create_retention_policy(name=db_name + '_ret_policy',
+                                                  duration=retention_policies[db_name],
+                                                  replication='1')
+            logger.info(f'Retention policy for {db_name} created')
 
 influx_client.close()
