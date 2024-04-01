cd0d81412a60d2d9e7c623acfb5e5ca96255c636
fry1231 <sobaka.samsobaka@yandex.ru>
2024-04-01 13:25:47 +0200

    [Fix] Redis conn pool reuse

--- app/db/influx/client.py
+++ app/db/influx/client.py
@@ -10,6 +10,8 @@ def get_influx_client(db_name: str):
                                             password=INFLUXDB_PASSWORD,
                                             database=db_name)
     try:
+        logger.debug(f"Opening influx client for {db_name}")
         return client
     finally:
+        logger.debug(f"Closing influx client for {db_name}")
         client.close()
--- app/dependencies.py
+++ app/dependencies.py
@@ -20,8 +20,12 @@ home_client = lambda: get_influx_client('home')
 farm_client = lambda: get_influx_client('farm')
 
 
+n = 0
 # redis
 def get_redis_conn():
+    global n
+    n += 1
+    logger.debug(f"Got redis connection {n}")
     redis_conn = aioredis.Redis(connection_pool=redis_pool, decode_responses=True)
     return redis_conn
 
