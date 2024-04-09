a9303ad3612693b7e9fdd655f60700605ddc65a3
fry1231 <sobaka.samsobaka@yandex.ru>
2024-04-09 21:39:59 +0200

    [Minor] gql testing

--- app/routers/graphql.py
+++ app/routers/graphql.py
@@ -233,18 +233,18 @@ class Query:
         return [User.from_orm(user) for user in users]
 
     @strawberry.field(permission_classes=[IsAuthenticated])
-    async def paincases(self, id_: int) -> PainCase:
-        paincase = await OrmarPainCase.objects.filter(id=id_).first()
+    async def paincases(self, id: int) -> PainCase:
+        paincase = await OrmarPainCase.objects.filter(id=id).first()
         return PainCase.from_orm(paincase)
 
     @strawberry.field(permission_classes=[IsAuthenticated])
-    async def pressures(self, id_: int) -> Pressure:
-        pressure = await OrmarPressure.objects.filter(id=id_).first()
+    async def pressures(self, id: int) -> Pressure:
+        pressure = await OrmarPressure.objects.filter(id=id).first()
         return Pressure.from_orm(pressure)
 
     @strawberry.field(permission_classes=[IsAuthenticated])
-    async def druguses(self, id_: int) -> DrugUse:
-        druguse = await OrmarDrugUse.objects.filter(id=id_).first()
+    async def druguses(self, id: int) -> DrugUse:
+        druguse = await OrmarDrugUse.objects.filter(id=id).first()
         return DrugUse.from_orm(druguse)
 
     @strawberry.field(permission_classes=[IsAuthenticated])
