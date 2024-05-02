c70fdf185c2f7e6ab4d19c5f0b079ff70e877db4
fry1231 <sobaka.samsobaka@yandex.ru>
2024-05-03 01:09:04 +0200

    [Fix] injectable dependencies fake query params

--- app/misc/dependencies.py
+++ app/misc/dependencies.py
@@ -57,11 +57,8 @@ def injectable(
 
         param_names = inspect.signature(func).parameters.keys()
         fake_query_params = {k: v for k, v in zip(param_names, args)}
-        logger.debug(f"Resolving dependencies for function {func}, args: {args}, kwargs: {kwargs}, dict: {fake_query_params}")
-        if fake_query_params:
-            fake_query_params.update({"self": None})
-        logger.debug(f"Fake query params: {fake_query_params}")
         logger.debug(f"Resolving dependencies for function {func}, args: {args}, kwargs: {kwargs}")
+        logger.debug(f"Fake query params: {fake_query_params}")
         fake_request = Request({"type": "http", "headers": [], "query_string": fake_query_params})
         values: dict[str, Any] = {}
         errors: list[Any] = []
