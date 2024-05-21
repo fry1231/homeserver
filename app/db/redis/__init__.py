41359d49933562fca0dc7505c286d2f788dd93f5
fry1231 <sobaka.samsobaka@yandex.ru>
2024-05-21 17:47:15 +0200

    [Feature] user maps

--- app/db/redis/__init__.py
+++ app/db/redis/__init__.py
@@ -2,7 +2,7 @@ from typing import AsyncIterator
 import aioredis
 
 
-async def init_redis_pool():
+def init_redis_pool():
     return aioredis.ConnectionPool.from_url("redis://redis", db=1, max_connections=100, decode_responses=True)
 
 
