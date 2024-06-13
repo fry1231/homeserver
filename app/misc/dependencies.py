0ddaef01ae9e006fead7244782b2573e708b1dbe
fry1231 <sobaka.samsobaka@yandex.ru>
2024-06-13 22:37:56 +0200

    redis pool debugging

--- app/misc/dependencies.py
+++ app/misc/dependencies.py
@@ -100,10 +100,9 @@ def farm_client():
 async def get_redis_conn():
     redis_conn = aioredis.Redis(connection_pool=redis_pool, decode_responses=True)
     t = time()
-    logger.debug(f'Redis conn #{t} executed in {inspect.stack()[3].function} at {inspect.stack()[3].filename}')
-    logger.debug(f'Stack: {[el.function for el in inspect.stack()]}')
+    logger.debug(f'Redis conn #{t}')
     try:
         yield redis_conn
     finally:
         await redis_conn.close()
-        logger.debug(f'Redis conn #{t} released in {inspect.stack()[3].function} at {inspect.stack()[3].filename}')
+        logger.debug(f'Redis conn #{t} released')
