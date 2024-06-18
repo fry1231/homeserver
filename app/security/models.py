e9ee63416f6e41429fd03a1e500f7e184b43aa6c
fry1231 <sobaka.samsobaka@yandex.ru>
2024-06-18 02:13:06 +0200

    Prev refresh token invalidation

--- app/security/models.py
+++ app/security/models.py
@@ -64,7 +64,7 @@ class TokensResponse(ORJSONResponse):
             }
         )
         self.set_cookie(
-            domain=DOMAIN,
+            domain=f'hs.{DOMAIN}',
             key="refresh_token",
             value=refresh_token,
             httponly=False,
